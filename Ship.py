eece07e16294a586aee42dcec0675a2ac0aff06b
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-01-24 08:31:28 +0100

    refactore main class

--- Ship.py
+++ Ship.py
@@ -23,10 +23,10 @@ class Ship:
 
     def update(self):
         """Update The ships position based on the movement flag"""
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
-            self.rect.x -= self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
 
         # update rect object from self.x
         self.rect.x = self.x
